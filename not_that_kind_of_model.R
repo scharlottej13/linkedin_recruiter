d1f67c06c9fcbc0ddd1095e9a6875a2716a73b17
Sarah Johnson <scharlottej13@gmail.com>
2021-02-01 15:42:58 +0100

    don't need to do this in R anymore

--- not_that_kind_of_model.R
+++ not_that_kind_of_model.R
@@ -15,17 +15,7 @@ df <- read.csv("/Users/scharlottej13/Nextcloud/linkedin_recruiter/outputs/model_
 # just pick one date for now
 df <- subset(df, query_date == "2020-07-25")
 
-fix_data <- function(df) {
-  # this will probably change as covariates change
-  numeric_cols <- c("flow", "users_orig", "users_dest", "area_orig", "area_dest", "distance")
-  # need to drop 0s, NaNs, and make sure they are numeric
-  df <- df[complete.cases(df[,numeric_cols]),]
-  df <- df[apply(df[numeric_cols] > 0, 1, all),]
-  # there must be an easier way to do this? how to get column index number from column name?
-  df <- cbind(df[,-which(names(df) %in% numeric_cols)], apply(df[numeric_cols], 2, as.numeric))
-}
-
-run_cohen_model <- function(df, x_vars, categ_vars = c()) {
+run_cohen_model <- function(df, x_vars, categ_vars = c(), threshold=1) {
   # create indicator variables (matrix of 0s/1s)
   wide_vars <- as.data.frame(model.matrix(~ country_dest + country_orig - 1, data=df))
   df[,c(x_vars, "flow")] <- log10(df[,c(x_vars, "flow")])
