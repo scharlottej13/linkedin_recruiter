0cf37e3560f7fe1d5b631dd02f253b923b5470df
Sarah Johnson <scharlottej13@gmail.com>
2021-02-03 17:05:51 +0100

    tried more models, still just as confused

--- not_that_kind_of_model.R
+++ not_that_kind_of_model.R
@@ -1,42 +1,50 @@
 library(MASS)
+# install.packages("~/Users/scharlottej13/repos/gravity")
 
-# TO DO
-# make it easy to swap between mac/windows
-#pc <- path.abspath("N:/johnson/linkedin_recruiter")
-#mac <- path.abspath("/Users/scharlottej13/Nextcloud/linkedin_recruiter")
-#if (Sys.getenv("HOME") == "/Users/scharlottej13")
-#{parent_dir <- "/Users/scharlottej13/Nextcloud/linkedin_recruiter"}
-
-# read in data (thanks Tom!)
-df <- read.csv("/Users/scharlottej13/Nextcloud/linkedin_recruiter/outputs/model_input_2021-02-01.csv")
+# read in data
+df <- read.csv("/Users/scharlottej13/Nextcloud/linkedin_recruiter/outputs/model_input_2021-02-03.csv")
 # just pick one date for now
 df <- subset(df, query_date == "2020-07-25")
+# quick test to see if this helps other important variables not drop out
+# tl;dr did not help
+keep_isos <- c("usa", "can", "fra", "gbr", "uae")
+df <- df %>% filter(iso3_orig %in% keep_isos & iso3_dest %in% keep_isos)
 
-run_cohen_model <- function(df, x_vars, categ_vars = c(), threshold=1) {
+prep_data <- function(df, x_vars, categ_vars = c(), threshold = 1) {
   # drop small flow values, arbitrary threshold
   df <- df[df$flow > threshold, ]
   # drop rows with null values if column is a covariate
-  df <- df[complete.cases(df[,x_vars]),]
-  # create indicator variables (matrix of 0s/1s)
+  df <- df[complete.cases(df[,c(x_vars, categ_vars)]),]
+  # create indicator variables (matrix of 0s/1s); -1 drops intercept column
+  # this is how it was done in cohen paper; in a test dataset I compared
+  # this to factor(country_dest) + factor(country_orig) and though fitted values
+  # were identical, the beta coefficients were different
   wide_vars <- as.data.frame(model.matrix(~ country_dest + country_orig - 1, data=df))
-  # log10 transform
-  # nice post on when to log t'form:
-  # https://stats.stackexchange.com/questions/298/in-linear-regression-when-is-it-appropriate-to-use-the-log-of-an-independent-va
-  df[,c(x_vars, "flow")] <- log10(df[,c(x_vars, "flow")])
   if (length(categ_vars) > 0) {df[,categ_vars] <- factor(df[,categ_vars])}
   df <- cbind(wide_vars, df[,c(x_vars, "flow", categ_vars)])
-  fit <- lm(flow ~ ., data = df)
 }
 
-fit1 <- run_cohen_model(df, c("users_orig", "users_dest", "distance"))
-fit2 <- run_cohen_model(df, c("users_orig", "users_dest", "distance", "area_orig", "area_dest"))
-fit3 <- run_cohen_model(df, c("users_orig", "users_dest", "distance", "area_orig"))
-fit4 <- run_cohen_model(df, c("users_orig", "users_dest", "distance", "area_dest"))
-fit5 <- run_cohen_model(df, c("users_orig", "users_dest", "distance", "population_dest", "population_orig"))
-fit6 <- run_cohen_model(df, c("distance", "population_dest", "population_orig"))
+run_model <- function(df, x_vars, type = "cohen", log_vars = x_vars,
+                      categ_vars = c(), threshold = 1) {
+  x_vars <- union(x_vars, log_vars)
+  df <- prep_data(df, x_vars, categ_vars, threshold)
+  if (type == "cohen") {
+    # https://www.pnas.org/content/105/40/15269
+    df[,c(log_vars, "flow")] <- log10(df[,c(log_vars, "flow")])
+    fit <- lm(flow ~ ., data = df)
+  } else if(type == "poisson") {
+    df[,c(log_vars)] <- log(df[,c(log_vars)])
+    fit <- glm(flow ~ ., family = poisson(), data = df)
+  } else { print("Model type needs to be one of 'cohen' or 'poisson''") }
+}
 
-# see if indicator variables way is same as factor variables-- not quite, but similar
-lm(log10(flow) ~ factor(country_dest) + factor(country_orig) + log10(distance) + log10(users_orig) + log10(users_dest), data = df)
+fit <- run_model(df, c("users_orig", "users_dest"))
+fit1 <- run_model(df, c("users_orig", "users_dest", "distance"))
+fit2 <- run_model(df, c("users_orig", "users_dest", "distance", "area_orig", "area_dest"))
+fit3 <- run_model(df, c("users_orig", "users_dest", "distance", "area_orig"))
+fit4 <- run_model(df, c("users_orig", "users_dest", "distance", "area_dest"))
+fit5 <- run_model(df, c("internet_orig", "internet_dest"), log_vars = c("users_orig", "users_dest", "distance"))
+fit6 <- run_model(df, c("users_orig", "users_dest", "distance", "maxgdp_dest", "maxgdp_orig"))
 
 plot(model.frame(fit1)$flow, fit1$fitted.values)
 
@@ -49,6 +57,7 @@ plot(model.frame(fit1)$flow, fit1$fitted.values)
 # 2) find outliers
 # 3) understand meaning of coefficients
 # 4) how does using one-hot encoding differ from a categorical variable?
-# 5) add "labor market conditions" see Bijak et al. ref
+# 5) add "labor market conditions"? see Bijak et al. ref
 # 6) replace w/ population "weighted average" by age?
 # [^ maybe useful? perhaps if the linkedin users are younger then they are also more likely to have aspirations?]
+
