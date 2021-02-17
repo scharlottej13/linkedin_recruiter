d515ef4edceda418ec8a0caa86808679ff042c33
Sarah Johnson <scharlottej13@gmail.com>
2021-02-17 10:42:16 +0100

    incorporate into main modeling script

--- gravity.R
+++
@@ -1,7 +0,0 @@
-library(gravity)
-
-fit <- ddm(
-    dependent_variable = "flow", distance = "distance",
-    code_origin = "iso3_orig", code_destination = "iso3_dest",
-    data = select(df, flow, distance, iso3_orig, iso3_dest)
-)
