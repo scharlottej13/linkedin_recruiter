8926c8c6eae14b891f032f782ad08d182aa7de0b
Sarah Johnson <scharlottej13@gmail.com>
2021-05-04 18:03:00 +0200

    actually the code to reproduce these

--- viz/chord_graph.R
+++ viz/chord_graph.R
@@ -5,58 +5,97 @@
 library(circlize)
 library(dplyr)
 
-BASE_DIR <- normalizePath("/Users/scharlottej13/Nextcloud/linkedin_recruiter/")
-
-df1 <- read.csv(normalizePath(paste0(
-  BASE_DIR, "/raw-data/chord_labels_colors.csv"))) %>% arrange(order1)
-color_vector <- setNames(df1$col1, df1$region)
-
-df <- read.csv(normalizePath(paste0(
-  BASE_DIR, "/processed-data/chord_diagram_midreg.csv")))
-df$flow <- df$flow / 100000
-
-chordDiagram(x = df)
-circos.clear()
-circos.par(start.degree = 90, gap.degree = 4, track.margin = c(-0.1, 0.1),
-           points.overflow.warning = FALSE)
-chordDiagram(
-  x = df, grid.col = color_vector, transparency = 0.25, order = df1$region,
-  directional = 1, direction.type = c("arrows", "diffHeight"),
-  diffHeight  = -0.04, annotationTrack = "grid",
-  link.arr.type = "big.arrow", link.sort = TRUE, link.largest.ontop = TRUE)
-
-circos.trackPlotRegion(
-  track.index = 1,
-  bg.border = NA,
-  panel.fun = function(x, y) {
-    # function loops through each 'sector.index'
-    xlim <- get.cell.meta.data("xlim")
-    print(xlim)
-    sector_index <- get.cell.meta.data("sector.index")
-    # need this since labels differ from sector.index values
-    reg1 <- df1$reg1[df1$region == sector_index]
-    reg2 <- df1$reg2[df1$region == sector_index]
-    # add text for reg1
-    circos.text(
-      x = mean(xlim), y = ifelse(test = nchar(reg2) == 0, yes = 5.2, no = 6),
-      labels = reg1, facing = "bending", cex = 1.4
-    )
-    # add text for reg2
-    circos.text(x = mean(xlim), y = 4.4,
-                labels = reg2, facing = "bending", cex = 1.4)
-    # Add ticks & labels
-    circos.axis(
-      h = "top",
-      major.at = seq(from = 0, to = xlim[2],
-                     by = ifelse(test = xlim[2] > 10, yes = 2, no = 1)),
-      minor.ticks = 1, labels.niceFacing = FALSE)
+get_parent_dir <- function() {
+  os <- Sys.info()[["sysname"]]
+  if (os == "Windows") {
+    parent_dir <- "N:\\johnson\\linkedin_recruiter"
+  } else {
+    parent_dir <- "/Users/scharlottej13/Nextcloud/linkedin_recruiter"
   }
-)
-filepaths <- c(
-  normalizePath(paste0(
-  BASE_DIR, "/plots/_archive/chord_diagram_region", "_", Sys.Date(), ".pdf")),
-  normalizePath(paste0(BASE_DIR, "/plots/chord_diagram_region.pdf"))
-)
-for (filename in filepaths) {
-  dev.copy2pdf(file = filename, height = 12, width = 15)
+  normalizePath(parent_dir, mustWork = TRUE)
 }
+
+plot_n_save_wrapper <- function(df, df1, color_vector,
+ base_dir, recip = FALSE) {
+  circos.clear()
+  circos.par(start.degree = 90, gap.degree = 4, track.margin = c(-0.1, 0.1),
+            points.overflow.warning = FALSE)
+  chordDiagram(
+    x = df, grid.col = color_vector, transparency = 0.25, order = df1$region,
+    directional = 1, direction.type = c("arrows", "diffHeight"),
+    diffHeight  = -0.04, annotationTrack = "grid",
+    link.arr.type = "big.arrow", link.sort = TRUE, link.largest.ontop = TRUE)
+  circos.trackPlotRegion(
+    track.index = 1,
+    bg.border = NA,
+    panel.fun = function(x, y) {
+      # function loops through each 'sector.index'
+      xlim <- get.cell.meta.data("xlim")
+      print(xlim)
+      sector_index <- get.cell.meta.data("sector.index")
+      # need this since labels differ from sector.index values
+      reg1 <- df1$reg1[df1$region == sector_index]
+      reg2 <- df1$reg2[df1$region == sector_index]
+      # add text for reg1
+      circos.text(
+        x = mean(xlim), y = ifelse(test = nchar(reg2) == 0, yes = 5.2, no = 6),
+        labels = reg1, facing = "bending", cex = 1.4
+      )
+      # add text for reg2
+      circos.text(x = mean(xlim), y = 4.4,
+                  labels = reg2, facing = "bending", cex = 1.4)
+      # Add ticks & labels
+      circos.axis(
+        h = "top",
+        major.at = seq(from = 0, to = xlim[2],
+                      by = ifelse(test = xlim[2] > 10, yes = 2, no = 1)),
+        minor.ticks = 1, labels.niceFacing = FALSE)
+    }
+  )
+  # TODO clean up this if block
+  if (recip) {
+    filepaths <- c(
+      normalizePath(
+        file.path(
+          base_dir, "plots", "recip", "_archive",
+          paste0("chord_diagram_region", "_", Sys.Date(), ".pdf")
+        ),
+        normalizePath(file.path(
+          base_dir, "plots", "recip", "chord_diagram_region.pdf"))
+      )
+    )
+  } else {
+    filepaths <- c(
+      normalizePath(
+        file.path(
+          base_dir, "plots", "_archive",
+          paste0("chord_diagram_region", "_", Sys.Date(), ".pdf")
+        ),
+        normalizePath(file.path(base_dir, "plots", "chord_diagram_region.pdf"))
+      )
+    )
+  }
+  for (filename in filepaths) {
+    dev.copy2pdf(file = filename, height = 12, width = 15)
+  }
+}
+
+# flexible for windows or linux os
+base_dir <- get_parent_dir()
+# for colors & order of the panels
+df1 <- read.csv(file.path(base_dir, "raw-data", "chord_labels_colors.csv")
+  ) %>% arrange(order1)
+color_vector <- setNames(df1$col1, df1$region)
+# read in bilateral flows of 'prospective relocators' data
+df <- read.csv(file.path(
+  base_dir, "processed-data", "chord_diagram_midreg.csv"))
+df$flow <- df$flow / 100000
+# save circle sankey
+plot_n_save_wrapper(df, df1, color_vector, base_dir)
+# read in bilateral flows of 'prospective relocators' data
+df <- read.csv(file.path(
+  base_dir, "processed-data", "chord_diagram_midreg_recip.csv"
+))
+df$flow <- df$flow / 100000
+# save circle sankey
+plot_n_save_wrapper(df, df1, color_vector, base_dir, recip = TRUE)
