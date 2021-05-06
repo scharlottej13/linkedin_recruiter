d0506e1a333d21d19b5d0c89c4f5c7c500bac2cb
Sarah Johnson <scharlottej13@gmail.com>
2021-05-06 16:06:00 +0200

    updates to model

--- model/not_that_kind_of_model.R
+++ model/not_that_kind_of_model.R
@@ -1,5 +1,6 @@
 library(MASS)
 library(dplyr)
+library(tidyr)
 library(gravity)
 
 get_parent_dir <- function() {
@@ -12,13 +13,30 @@ get_parent_dir <- function() {
   normalizePath(parent_dir, mustWork = TRUE)
 }
 
+save_to_archive <- function(origname, active_dir) {
+  archive_dir <- file.path(active_dir, "_archive")
+  split_name <- unlist(strsplit("xxx.csv", ".", fixed = T))
+  newname <- paste0(split_name[1], "_", Sys.Date(), ".", split_name[2])
+  file.copy(file.path(active_dir, origname), archive_dir)
+  file.rename(from = file.path(archive_dir, origname),
+    to = file.path(archive_dir, newname))
+}
+
 return_df <- function(df) {
   # TODO does R have a builtin f'n for this?
   df
 }
 
 prep_data <- function(
-  df, dep_var, factor_vars, log_vars, keep_vars, type, min_n) {
+  df, dep_var, factor_vars, log_vars, keep_vars, type, min_n,
+  min_dest_prop, global) {
+  if (!global) {
+    df <- df %>%
+      filter(eu_plus == 1)
+  } else {
+    df <- df %>%
+      filter(users_orig_median > min_dest_prop)
+  }
   if (type == "cohen") {
     log_vars <- c(dep_var, log_vars)
     my_func <- "log10"
@@ -33,12 +51,11 @@ prep_data <- function(
     filter(dep_var >= min_n) %>%
     mutate_at(factor_vars, factor) %>%
     mutate_at(log_vars, getFunction(my_func)) %>%
-    dplyr::select(all_of(keep_vars))
+    dplyr::select(all_of(c(keep_vars, "country_dest", "country_orig"))) %>%
+    drop_na()
 }
 
-run_model <- function(
-  df, dep_var = "flow", log_vars = NULL, factors = NULL,
-  other_numeric = NULL, type = "cohen", min_n=25) {
+run_model <- function(df, dep_var, type, keep_vars) {
   # Run a model of flow ~ country_destination + country_origin
   # log_vars: independent variables that will be log transformed
   # other_factors: independent categorical variables in addition to
@@ -46,8 +63,6 @@ run_model <- function(
   # other_numeric: independent numeric variables that are not log transformed
   # type: type of model to run, one of cohen, poisson, or gravity
   # min_n: minimum value for the count of the dependent variable
-  keep_vars <- unique(c(dep_var, log_vars, factors, other_numeric))
-  df <- prep_data(df, dep_var, factors, log_vars, keep_vars, type, min_n)
   formula <- as.formula(paste(
     dep_var, paste(keep_vars[keep_vars != dep_var],
     collapse = " + "), sep = " ~ "))
@@ -72,11 +87,7 @@ run_model <- function(
 }
 
 add_fit_quality <- function(fit, df) {
-  keep_vars <- unique(
-    c("country_dest", "country_orig"), attr(fit$terms, "term.labels")
-  )
   df %>%
-    select(keep_vars) %>%
     mutate(
       r2 = fit$r.squared,
       adj_r2 = fit$adj.r.squared,
@@ -88,47 +99,58 @@ add_fit_quality <- function(fit, df) {
     )
 }
 
-base_dir <- get_parent_dir()
-out_dir <- file.path(base_dir, "model-outputs")
-arch_dir <- file.path(base_dir, "model-outputs", "_archive")
-date <- Sys.Date()
+save_model <- function(
+  df, suffix, out_dir = file.path(BASE_DIR, "model-outputs"),
+  dep_var = "flow_median", log_vars = NULL, factors = NULL,
+  other_numeric = NULL, type = "cohen", min_n=25,
+  min_dest_prop = 0.05, global = FALSE
+) {
+    keep_vars <- unique(c(dep_var, log_vars, factors, other_numeric))
+    model_df <- prep_data(df, dep_var, factors, log_vars, keep_vars,
+      type, min_n, min_dest_prop, global)
+    fit <- run_model(model_df, dep_var, type, keep_vars)
+    filename <- paste0("cohen_", suffix)
+    sink(file.path(out_dir, paste0(filename, ".txt")))
+    print(summary(fit))
+    sink()
+    write.csv(add_fit_quality(fit, model_df),
+      file.path(out_dir, paste0(filename, ".csv")),
+      row.names = FALSE
+    )
+    for (file in c(paste0(filename, ".txt"), paste0(filename, ".csv"))) {
+      save_to_archive(file, out_dir)
+    }
+  }
+
+# get working directories
+BASE_DIR <- get_parent_dir()
 # read in data
-df <- read.csv(
-  file.path(base_dir, "processed-data", "variance.csv")
-) %>% filter(prop_dest_median > 0.05)
-# linear model
-fit <- run_model(
-  df,
-  dep_var = "flow_median",
-  log_vars = c("dist_biggest_cities", "users_orig_median", "users_dest_median"),
-  other_numeric = c("contig", "csl"),
-  factors = c("country_dest", "country_orig")
+df <- read.csv(file.path(BASE_DIR, "processed-data", "variance.csv"))
+save_model(df, "base_dist_pop_weighted_plus_comcol", global = TRUE,
+log_vars = c(
+  "dist_pop_weighted", "users_orig_median",
+  "users_dest_median", "area_dest", "area_orig"
+), other_numeric = c("csl", "contig", "comcol"))
+save_model(df, "base_dist_pop_weighted_plus_colony",
+  global = TRUE,
+  log_vars = c(
+    "dist_pop_weighted", "users_orig_median",
+    "users_dest_median", "area_dest", "area_orig"
+  ), other_numeric = c("csl", "contig", "colony")
 )
-sink(file.path(out_dir, "cohen_5.txt"))
-print(summary(fit))
-sink()
-write.csv(fit$model, file.path(out_dir, "cohen_modelframe_5.csv"),
-  row.names = FALSE
+save_model(df, "base_dist_pop_weighted_plus_col45",
+  global = TRUE,
+  log_vars = c(
+    "dist_pop_weighted", "users_orig_median",
+    "users_dest_median", "area_dest", "area_orig"
+  ), other_numeric = c("csl", "contig", "col45")
 )
-write.csv(
-  add_fit_quality(fit, df),
-  file.path(out_dir, "cohen_5.csv"), row.names = FALSE
-)
+
 # # poisson
 # fit2 <- run_model(df,
 #   log_vars = c("distance"), other_factors = c("query_date"),
 #   other_numeric = c("prop_users_orig", "prop_users_dest"), type = "poisson"
 # )
-# write.csv(add_fit_quality(fit2, df),
-#           gsub("model", "poisson_model", outpath), row.names = FALSE)
-
-# fit <- run_model(df,
-#   log_vars = c("distance"), type = "gravity"
-# )
-
-# df <- add_fit_quality(fit, df)
-# write.csv(df, gsub("input", "output", filepath), row.names = FALSE)
-
 # should I use outliers or coefficients to figure out which countries are interesting?
 # next steps:
 # understand meaning of coefficients
