9e037b2e178612316fc4d67502815d76be4b57df
Sarah Johnson <scharlottej13@gmail.com>
2021-09-14 18:09:21 +0200

    prettier plots

--- viz/flows_over_time.py
+++ viz/flows_over_time.py
@@ -5,22 +5,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from configurator import Config
+from utils.misc import custom_round
 
 CONFIG = Config()
-
-
-def check_cutoffs(df, y):
-    # check to make sure that countries
-    # don't appear twice across different plots
-    for idx in range(1, df.cutoff.max()):
-        overlap = \
-            set(df.query(f'cutoff == {idx - 1}')[f'country_{y}']) \
-            & set(df.query(f'cutoff == {idx}')[f'country_{y}'])
-        if len(overlap) != 0:
-            # this could be better & pick an index based on where more
-            # points are already
-            df.loc[df[f'country_{y}'].isin(overlap), 'cutoff'] = idx - 1
-    return df
 
 
 def lineplt_broken_y_axis(df, avg_n, avg_prop, x='orig', y='dest'):
@@ -88,7 +75,7 @@ def lineplt_broken_y_axis(df, avg_n, avg_prop, x='orig', y='dest'):
     plt.show()
 
 
-def line_plt(df, iso, avg_prop, avg_n, x, y, split=None, log_scale=False):
+def line_plt(df, iso, avg_prop, avg_n, x, y, suffix=None, log_scale=False):
     """Create time series line plot.
 
     df: dataframe of bilateral flows
@@ -99,8 +86,6 @@ def line_plt(df, iso, avg_prop, avg_n, x, y, split=None, log_scale=False):
     x, y: either 'orig' or 'dest', designed to easily run for either direction
         ie. show for a single origin all destination countries or
         for a single destination show all origin countries
-    split: if a value is passed, then the plot is formatted a bit differently
-        to account for the fact that not all corresponding countries are shown
     log_scale: whether to log transform the y-axis (open to relocate / users)
     """
     fig, ax = plt.subplots(figsize=(10, 5))
@@ -114,20 +99,13 @@ def line_plt(df, iso, avg_prop, avg_n, x, y, split=None, log_scale=False):
         ax=ax
     )
     # make adjustments
-    ax.set_xticklabels(df['query_date'].unique(),
-                       rotation=45, horizontalalignment='right')
+    ax.set_xticklabels(
+        df['query_date'].unique(), rotation=45, ha='right')
+    ax.set_yticklabels([f'{x*100000:.0f}' for x in ax.get_yticks().tolist()])
     ax.set_xlabel('Date of Data Collection')
-    ax.set_ylabel('Percent of all users')
-    if not split:
-        suffix = 'time_series_all'
-        ncol = 2
-        ax.set_yticklabels([f'{x:.2%}' for x in ax.get_yticks().tolist()])
-    else:
-        suffix = f'time_series_split_{split}'
-        ncol = 1
-        ax.set_yticklabels([f'{x:.3%}' for x in ax.get_yticks().tolist()])
+    ax.set_ylabel('Users per 100,000')
     if log_scale:
-        suffix = 'time_series_log_scale'
+        suffix = 'log_scale'
         ax.set_ylabel('Proportion (log10)')
     # ok now format some text
     country = df[f'country_{x}'].values[0]
@@ -139,7 +117,7 @@ def line_plt(df, iso, avg_prop, avg_n, x, y, split=None, log_scale=False):
         title = f'LinkedIn Users Open to Relocating to {country}'
     ax.legend(
         bbox_to_anchor=(1.05, 1), loc=2,
-        title=legend, frameon=False, ncol=ncol)
+        title=legend, frameon=False, ncol=1)
     # main title
     ax.text(
         x=0.5, y=1.1, s=title, transform=ax.transAxes,
@@ -148,7 +126,7 @@ def line_plt(df, iso, avg_prop, avg_n, x, y, split=None, log_scale=False):
     # subtitle
     ax.text(
         x=0.5, y=1.03,
-        s=f'on average {avg_prop:.1%} of people in {country} use LinkedIn (n={avg_n:,.0f})',
+        s=f'on average {avg_prop:.1%} of people in {country} use LinkedIn (n={avg_n})',
         fontsize=10, alpha=0.75, ha='center', va='bottom',
         transform=ax.transAxes
     )
@@ -158,7 +136,7 @@ def line_plt(df, iso, avg_prop, avg_n, x, y, split=None, log_scale=False):
     if not path.exists(outdir):
         mkdir(outdir)
     plt.savefig(
-        f"{outdir}/{iso}_{x}_{suffix}.png", dpi=300, bbox_inches="tight")
+        f"{outdir}/{iso}_{x}_time_series_{suffix}.png", dpi=300, bbox_inches="tight")
     plt.close()
 
 
@@ -180,43 +158,46 @@ def prep_data(iso, x, y):
     ).sort_values(by=['query_date', 'prop'], ascending=[True, False])
 
 
+def get_top_countries(x, iso3_y, df, variance_df):
+    top = variance_df.sort_values(
+        by='flow_mean', ascending=False).iloc[:x][f'iso3_{iso3_y}'].values
+    top_df = df[df[f'iso3_{iso3_y}'].isin(top)]
+    # bit of a hack
+    # because of an earlier restriction (that we like) sometimes < 10
+    num_isos = x
+    while len(top_df[f'iso3_{iso3_y}'].unique()) < x:
+        num_isos += 1
+        top = variance_df.sort_values(
+            by='flow_mean', ascending=False
+        ).iloc[:num_isos][f'iso3_{iso3_y}'].values
+        top_df = df[df[f'iso3_{iso3_y}'].isin(top)]
+    return top_df
+
+
 def main(iso, dest):
     if not dest:
-        x = 'orig'
-        y = 'dest'
+        iso3_x = 'orig'
+        iso3_y = 'dest'
     else:
-        x = 'dest'
-        y = 'orig'
+        iso3_x = 'dest'
+        iso3_y = 'orig'
     # data that will be plotted
-    df = prep_data(iso, x, y)
+    df = prep_data(iso, iso3_x, iso3_y)
     # now pull in some other metrics
     variance_df = pd.read_csv(
         f"{CONFIG['directories.data']['processed']}/variance.csv"
-    ).query(f"iso3_{x} == '{iso}'")
+    ).query(f"iso3_{iso3_x} == '{iso}'")
     # safe to take the first value b/c we only care about country_x
     # proportion of population using LinkedIn (averaged over time)
-    prop = variance_df[f'prop_{x}_mean'].iloc[0]
+    prop = variance_df[f'prop_{iso3_x}_mean'].iloc[0]
     # number of LinkedIn users per country (averaged over time)
-    n = variance_df[f'users_{x}_mean'].iloc[0]
+    n = custom_round(variance_df[f'users_{iso3_x}_mean'].iloc[0])
     # plot top 10 countries
-    top10 = variance_df.sort_values(
-        by='flow_mean', ascending=False).iloc[:10][f'iso3_{y}'].values
-    top10_df = df[df[f'iso3_{y}'].isin(top10)]
-    # bit of a hack
-    # because of an earlier restriction (that we like) sometimes < 10
-    num_isos = 10
-    while len(top10_df[f'iso3_{y}'].unique()) < 10:
-        num_isos += 1
-        top10 = variance_df.sort_values(
-            by='flow_mean', ascending=False
-        ).iloc[:num_isos][f'iso3_{y}'].values
-        top10_df = df[df[f'iso3_{y}'].isin(top10)]
-    line_plt(top10_df, iso, prop, n, x, y, split='top10')
+    line_plt(get_top_countries(10, iso3_y, df, variance_df),
+             iso, prop, n, iso3_x, iso3_y, suffix='top10')
+    line_plt(get_top_countries(5, iso3_y, df, variance_df),
+             iso, prop, n, iso3_x, iso3_y, suffix='top5')
     # lineplt_broken_y_axis(df[df[f'iso3_{y}'].isin(top_10)], n, prop)
-    # plot all countries, split across figures
-    # df = check_cutoffs(df, y)
-    # for idx in range(max(df['cutoff']), 0, -1):
-    #     line_plt(df[df['cutoff'] == idx], iso, prop, n, x, y, split=idx + 1)
 
 
 if __name__ == "__main__":
