7f26f96a5a86bb23980c26d503f5e395d63b2b4b
Sarah Johnson <scharlottej13@gmail.com>
2021-09-14 18:11:54 +0200

    take out broken axis part, dont need it

--- viz/flows_over_time.py
+++ viz/flows_over_time.py
@@ -10,71 +10,6 @@ from utils.misc import custom_round
 CONFIG = Config()
 
 
-def lineplt_broken_y_axis(df, avg_n, avg_prop, x='orig', y='dest'):
-    """Same lineplot, but with broken axis.
-    https://matplotlib.org/stable/gallery/subplots_axes_and_figures/broken_axis.html
-    """
-    f, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10, 5))
-    # plot the same data on both axes
-    lineplot_kwargs = {
-        'x': 'query_date', 'y': 'prop', 'hue': f'country_{y}', 'legend': False,
-        'style': f'country_{y}', 'marker': 'o', 'data': df, 'ax': ax1}
-    sns.lineplot(**lineplot_kwargs)
-    lineplot_kwargs.update({'ax': ax2, 'legend': 'full'})
-    sns.lineplot(**lineplot_kwargs)
-
-    # zoom-in / limit the view to different portions of the data
-    ax1.set_ylim(0.0016, 0.0019)  # outliers only
-    ax2.set_ylim(0, .0009)  # most of the data
-
-    # hide the spines between ax and ax2
-    ax1.spines['bottom'].set_visible(False)
-    ax2.spines['top'].set_visible(False)
-    ax1.xaxis.set_ticks_position('none')
-    ax1.tick_params(labeltop=False)  # don't put tick labels at the top
-    ax2.xaxis.tick_bottom()
-    # format tick labels as a percent
-    ax1.set_yticklabels([f'{x:.3%}' for x in ax1.get_yticks().tolist()])
-    ax2.set_yticklabels([f'{x:.3%}' for x in ax2.get_yticks().tolist()])
-    ax2.set_xticklabels(df['query_date'].unique(),
-                        rotation=45, horizontalalignment='right')
-    ax2.set_xlabel('Date of Data Collection')
-    ax1.set_ylabel(' all users')
-    ax2.set_ylabel('Percent of')
-
-    country = df[f'country_{x}'].values[0]
-    # formatting the legend
-    legend = 'Destination Country'
-    ax2.legend(bbox_to_anchor=(1.05, 1), loc=2,
-               title=legend, frameon=False, ncol=1)
-    # formatting the title
-    title = f'LinkedIn Users in {country} Open to Relocation'
-    # main title
-    ax1.text(
-        x=0.5, y=1.1, s=title, transform=ax1.transAxes,
-        fontsize=12, weight='bold', ha='center', va='bottom'
-    )
-    # subtitle
-    ax1.text(
-        x=0.5, y=1.03,
-        s=f'on average {avg_prop:.1%} of people in {country} use LinkedIn (n={avg_n:,.0f})',
-        fontsize=10, alpha=0.75, ha='center', va='bottom',
-        transform=ax1.transAxes
-    )
-
-    # formatting the diagnoal lines
-    d = .5  # proportion of vertical to horizontal extent of the slanted line
-    kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12,
-                  linestyle="none", color='k', mec='k', mew=1, clip_on=False)
-    ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
-    ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
-    # adjust space between subplots
-    f.subplots_adjust(hspace=0.1)
-    f.tight_layout()
-
-    plt.show()
-
-
 def line_plt(df, iso, avg_prop, avg_n, x, y, suffix=None, log_scale=False):
     """Create time series line plot.
 
@@ -197,7 +132,6 @@ def main(iso, dest):
              iso, prop, n, iso3_x, iso3_y, suffix='top10')
     line_plt(get_top_countries(5, iso3_y, df, variance_df),
              iso, prop, n, iso3_x, iso3_y, suffix='top5')
-    # lineplt_broken_y_axis(df[df[f'iso3_{y}'].isin(top_10)], n, prop)
 
 
 if __name__ == "__main__":
