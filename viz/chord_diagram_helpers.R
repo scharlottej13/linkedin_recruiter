16cce8fca3b4bd3151fe78057761426ccfcb43b8
Sarah Johnson <scharlottej13@gmail.com>
2021-10-29 11:55:31 -0700

    the other way looks pretty bad, changing it back

--- viz/chord_diagram_helpers.R
+++ viz/chord_diagram_helpers.R
@@ -143,8 +143,8 @@ plot_n_save_wrapper <- function(
         circos.text(
           mean(xlim), 3.2,
           paste0(
-            prettyNum(round(total, -3), big.mark = ","),
-            " prospective relocators"),
+            "N = ",
+            prettyNum(round(total, -3), big.mark = ",")),
             # "(",
             # round((sum1 / num_users) * 100000),
             # " per 100,000 users)"),
